d9f4d33461f04f1831d70e255d8eb03eca63fe46
Nathan Uy <58688998+Nathan-Uy@users.noreply.github.com>
2024-05-24 18:00:58 +0800

    Update search.py

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,32 @@ def search_users(args):
     """
 
     # Implement search here!
+    id = args.get('id')
+    name = args.get('name')
+    age = args.get('age')
+    occupation = args.get('occupation')
 
-    return USERS
+    id_matched=[]
+    name_matched=[]
+    age_matched=[]
+    occupation_matched=[]
+
+    for user in USERS:
+        if id and user['id'] == id:
+            id_matched.append(user)
+        if name and name.lower() in user['name'].lower():
+            name_matched.append(user)
+        if age and (int(age) - 1 <= user['age']<=int(age)+1):
+            age_matched.append(user)
+        if occupation and occupation.lower() in user['occupation'].lower():
+            occupation_matched.append(user)
+
+    all_matched = list ({user['id']: user for user in (id_matched+name_matched+age_matched+occupation_matched)}.values()) #concatinate all user data but with no duplicated
+
+    all_matched.sort(key=lambda user :(
+        user in id_matched,
+        user in name_matched,
+        user in age_matched,
+        user in occupation_matched
+    ), reverse=True)#sort the values based on the prioroty of search parameters
+    return all_matched
